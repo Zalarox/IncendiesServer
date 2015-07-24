1e24054f48218e46cfced39053046213205a7a12
Keep Botting <keepbotting@pbot.org>
2015-07-24 12:08:12 -0400

    Removing the run.bat I accidentally committed.

--- run.bat.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title Server
-"C:\Program Files\Java\jdk\bin\java.exe" -Xmx1024m -cp bin;deps/poi.jar;deps/mysql.jar;deps/gtlvote.jar;deps/mina.jar;deps/slf4j.jar;deps/slf4j-nop.jar;deps/jython.jar;log4j-1.2.15.jar; main.GameEngine
-pause
